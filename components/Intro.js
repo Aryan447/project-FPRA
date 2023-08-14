7dda726f7dbda56e99915f1a6c820522e4c86ea0
Aryan447 <singharyan4477@gmail.com>
2023-08-14 10:48:29 +0530

    initial commit

---
+++ components/Intro.js
@@ -0,0 +1,10 @@
+export default function Intro() {
+    return (
+      <div className="mt-56 ml-12 font-extrabold text-4xl">
+          <div className="flex">
+          <h1 className="gradient gradient-text font-inter">Fake Product's Review Analyzer</h1>
+          </div>
+          <h3 className="font-inter mt-2 text-lg">Spotlights authenticity, unmasking opinions. Fake reviews? It knows the truth.</h3>
+      </div>
+    )
+  }
