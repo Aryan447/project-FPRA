9b478c14f2ab58bd3e1642ef24d8e98bd2d22ca4
Aryan447 <singharyan4477@gmail.com>
2023-08-14 11:16:13 +0530

    initial commit

--- components/Intro.js
+++ components/Intro.js
@@ -1,14 +1,11 @@
-// Spotlights authenticity, unmasking opinions. Fake reviews? It knows the truth.
-
 export default function Intro() {
-    return (
-      <div className="mt-56 ml-12 font-extrabold text-4xl">
-          <div className="flex">
-          <h1 className="gradient gradient-text font-inter">Fake Product's Reviews Analyzer</h1>
-          </div>
-          <h1 className="font-inter mt-2 text-xl">Spotlights authenticity, unmasking opinions</h1>
-          <h1 className="font-inter text-xl">Fake reviews? It knows the truth.</h1>
+  return (
+    <div className="mt-20 ml-4 md:mt-56 md:ml-12 font-extrabold text-4xl">
+      <div className="flex">
+        <h1 className="gradient gradient-text font-inter md:text-5xl">Fake Product's Reviews Analyzer</h1>
       </div>
-    )
-  }
-  
+      <h1 className="font-inter mt-2 text-xl">Spotlights authenticity, unmasking opinions</h1>
+      <h1 className="font-inter text-xl">Fake reviews? It knows the truth.</h1>
+    </div>
+  )
+}
